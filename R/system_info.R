9743f3167e72a4a165c453babca047af03cd7d26
skgrange <skgrange@gmail.com>
2017-06-02 10:45:58 +0100

    add the r package

---
+++ R/system_info.R
@@ -0,0 +1,15 @@
+#' Function to return a formated \code{Sys.info} call. 
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @return Data frame. 
+#' 
+#' @export
+system_info <- function() {
+  
+  data.frame(
+    t(Sys.info()), 
+    stringsAsFactors = FALSE
+  )
+  
+}
