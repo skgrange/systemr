0233482de2574e0004983938cd4f2b85ba81bdc9
skgrange <skgrange@gmail.com>
2018-05-09 10:10:51 +0100

    add system_who function

---
+++ R/system_who.R
@@ -0,0 +1,36 @@
+#' Function to return logged in users. 
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @return Data frame. 
+#' 
+#' @export
+system_who <- function() {
+  
+  # System call
+  text <- system("who", intern = TRUE)
+  
+  # Clean return 
+  text <- stringr::str_squish(text)
+  list_text <- stringr::str_split(text, " |\\(|\\)")
+  list_text <- purrr::map(list_text, ~.x[.x != ""])
+  
+  # Extract and clean variables
+  name <- purrr::map_chr(list_text, 1)
+  line <- purrr::map_chr(list_text, 2)
+  time <- purrr::map_chr(list_text, ~stringr::str_c(.x[3], " ", .x[4]))
+  time <- lubridate::ymd_hm(time, tz = "UTC")
+  comment <- purrr::map_chr(list_text, 5)
+  
+  # Build data frame
+  df <- data.frame(
+    name, 
+    line,
+    time, 
+    comment,
+    stringsAsFactors = FALSE
+  )
+  
+  return(df)
+  
+}
