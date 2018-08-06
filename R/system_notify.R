7de9e614aa889423b1681693e4520ff5613034db
skgrange <s.k.grange@gmail.com>
2018-08-06 20:54:59 +0100

    :sparkles: enhance system_notify

--- R/system_notify.R
+++ R/system_notify.R
@@ -1,25 +1,33 @@
 #' Function to send a system notification to the Ubuntu desktop.
 #' 
+#' @param urgency Urgency of message, one of \code{"low"}, \code{"normal"}, or
+#' \code{"critical"}. Default is \code{"critical"} because it is the most 
+#' supported notification type. 
+#' 
 #' @param title Title for notification. 
 #' 
 #' @param message Message for notification.
 #' 
 #' @author Stuart K. Grange
 #' 
-#' @return Invisible.
+#' @return Invisible, a desktop notification. 
 #' 
 #' @export
-system_notify <- function(title = "R", message = "") {
+system_notify <- function(urgency = "critical", title = "R: ", message = "") {
   
   # Check
   stopifnot(ubuntu_test())
+  stopifnot(urgency %in% c("low", "normal", "critical"))
   
-  # Build command
+  # Build command pieces
+  command_notify <- stringr::str_c("notify-send -u ", urgency)
   title <- stringr::str_c('"', title, '"')
   message <- stringr::str_c('"', message, '"')
-  x <- stringr::str_c("notify-send -u critical", title, message, sep = " ")
+  
+  # Combine
+  command_notify <- stringr::str_c(command_notify, title, message, sep = " ")
   
   # System call
-  system(x)
+  system(command_notify)
   
 }
