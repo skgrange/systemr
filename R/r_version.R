ce8970867b1497f0aca2ba8c503d7510f9467f57
skgrange <s.k.grange@gmail.com>
2019-02-26 13:53:30 +0000

    :sparkles: enhance r_version

--- R/r_version.R
+++ R/r_version.R
@@ -2,34 +2,25 @@
 #' 
 #' @author Stuart K. Grange
 #' 
-#' @return Tibble. 
+#' @return Tibble with one row. 
 #' 
 #' @export
 r_version <- function() {
   
   # Get version
-  df <- data.frame(t(R.Version()))
+  list_version <- R.Version()
+  
+  # Build data frame
+  df <- threadr::build_zero_row_tibble(names(list_version))
+  df[1, ] <- as.character(list_version)
   
   # Clean names
-  names(df) <- stringr::str_replace_all(names(df), "\\.", "_")
+  names(df) <- stringr::str_replace_all(names(df), "\\.| ", "_")
   
-  # Add some formats too
-  df$version_string_short <- stringr::str_c(
-    df$language, 
-    " ", 
-    df$major, 
-    ".", 
-    df$minor
-  )
-  
-  df$version_string_nickname <- stringr::str_c(
-    df$version_string_short,
-    " ", 
-    df$nickname
-  )
-  
-  # To tibble
-  df <- as_tibble(df)
+  # Add some formatted strings too
+  df <- df %>% 
+    mutate(version_string_short = stringr::str_c(language, " ", major, ".", minor),
+           version_string_short = stringr::str_c(version_string_short, " ", nickname))
   
   return(df)
   
--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,8 @@ if (getRversion() >= "2.15.1") {
   
   # What variables are causing issues?
   variables <- c(
-    "variable", "date_start", "value", "bus", "device"
+    "variable", "date_start", "value", "bus", "device", "language", "major", 
+    "minor", "version_string_short", "nickname"
   )
   
   # Squash the note
