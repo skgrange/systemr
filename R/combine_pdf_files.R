dc7de057e82bb327e5ef02a7595addb98d93b224
skgrange <s.k.grange@gmail.com>
2020-01-11 07:58:44 +0100

    :sparkles: add combine_pdf_files

---
+++ R/combine_pdf_files.R
@@ -0,0 +1,36 @@
+#' Function to combine \code{pdf} files using \code{pdftk}.
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @param file Vector of file names to be combined. If not used, all files in 
+#' working directory will be combined. 
+#' 
+#' @param file_output Output file name. 
+#' 
+#' @param verbose Should the function give messages? 
+#' 
+#' @return Invisible. 
+#' 
+#' @export
+combine_pdf_files <- function(file = NA, file_output = "combined_file.pdf",
+                              verbose = FALSE) {
+  
+  # Parse output
+  file_output <- shQuote(file_output)
+  
+  # Build commands
+  if (!is.na(file[1])) {
+    file <- shQuote(file)
+    file <- stringr::str_c(file, collapse = " ")
+    command <- stringr::str_c("pdftk ", file, " cat output ", file_output)
+  } else {
+    command <- stringr::str_c("pdftk *.pdf cat output ", file_output)
+  }
+  
+  # Run command
+  if (verbose) message(threadr::date_message(), command)
+  system(command)
+  
+  return(invisible(command))
+  
+}
