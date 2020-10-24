6702f4e9475e3498d5bcc8ad8d820dd26b7bcdba
skgrange <s.k.grange@gmail.com>
2020-10-24 19:56:51 +0200

    :sparkles: add speed_test function

---
+++ R/speed_test.R
@@ -0,0 +1,61 @@
+#' Function to run the \code{speedtest-cli} internet bandwidth programme.
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @param verbose Should the function give messages? 
+#' 
+#' @return Tibble.
+#' 
+#' @export
+speed_test <- function(verbose = TRUE) {
+  
+  # To-do: Test for programme
+  
+  # Get system date
+  date_system <- lubridate::now()
+  
+  # Run testing programme
+  list_speed <- tryCatch({
+    processx::run("speedtest", echo = verbose)
+  }, error = function(e) {
+    NULL
+  })
+  
+  # If fails return here
+  if (is.null(list_speed)) return(tibble())
+  
+  # Format text output
+  text_output <- list_speed$stdout %>% 
+    stringr::str_split("\n") %>% 
+    .[[1]]
+  
+  # Format the pieces
+  text_download <- text_output %>% 
+    stringr::str_subset("Download") %>% 
+    stringr::str_split_fixed(": | ", 3)
+  
+  text_upload <- text_output %>% 
+    stringr::str_subset("Upload") %>% 
+    stringr::str_split_fixed(": | ", 3)
+  
+  host <- text_output %>% 
+    stringr::str_subset("Testing from") %>% 
+    stringr::str_remove_all("Testing from |\\.\\.\\.")
+  
+  server <- stringr::str_subset(text_output, "Hosted by")
+  
+  # Build tibble
+  df <- tibble(
+    date = date_system,
+    system = hostname(),
+    host = host,
+    server = server,
+    download = as.numeric(text_download[, 2]),
+    download_unit = text_download[, 3],
+    upload = as.numeric(text_upload[, 2]),
+    upload_unit = text_upload[, 3]
+  )
+  
+  return(df)
+  
+}
--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,7 @@ if (getRversion() >= "2.15.1") {
   
   # What variables are causing issues?
   variables <- c(
-    "variable", "date_start", "value", "bus", "device", "language", "major", 
+    ".", "variable", "date_start", "value", "bus", "device", "language", "major", 
     "minor", "version_string_short", "nickname"
   )
   
