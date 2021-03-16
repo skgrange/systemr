0ef1c09a9f4512860dd5cfdece9223207fce89ae
skgrange <s.k.grange@gmail.com>
2021-03-16 14:18:43 +0100

    :sparkles: add a max argument to cpu counter

--- R/system_cpu_core_count.R
+++ R/system_cpu_core_count.R
@@ -4,6 +4,21 @@
 #' 
 #' @param logical_cores Should logical cores be included in the core count? 
 #' 
+#' @param max_cores Should the return have a maximum value? This can be useful 
+#' when there are hundreds of cores and logic is being built. 
+#' 
+#' @return Integer vector with length of 1. 
+#' 
+#' @examples 
+#' # Find core count
+#' system_cpu_core_count()
+#' 
 #' @export
-system_cpu_core_count <- function(logical_cores = TRUE) 
-  parallel::detectCores(logical = logical_cores)
+system_cpu_core_count <- function(logical_cores = TRUE, max_cores = NA) {
+  x <- as.integer(parallel::detectCores(logical = logical_cores))
+  if (!is.na(max_cores)) {
+    max_cores <- as.integer(max_cores)
+    x <- if_else(x >= max_cores, max_cores, x) 
+  }
+  return(x)
+}
