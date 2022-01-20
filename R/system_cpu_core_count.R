7b11db09b5f57b73a50627f58dca9ad3e8dfcbd5
skgrange <s.k.grange@gmail.com>
2022-01-20 09:05:07 +0100

    :book: minor changes to system_cpu_core_count documentation

--- R/system_cpu_core_count.R
+++ R/system_cpu_core_count.R
@@ -5,11 +5,12 @@
 #' @param logical_cores Should logical cores be included in the core count? 
 #' 
 #' @param max_cores Should the return have a maximum value? This can be useful 
-#' when there are hundreds of cores and logic is being built. 
+#' when there are very many cores and logic is being built. 
 #' 
 #' @return Integer vector with length of 1. 
 #' 
 #' @examples 
+#' 
 #' # Find core count
 #' system_cpu_core_count()
 #' 
