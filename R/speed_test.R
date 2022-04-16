177afc859be5d45efab33a222b4d75b84339a016
skgrange <skgrange@gmail.com>
2022-04-16 10:54:58 +0200

    :sparkles: vectorise internet speed tester & add some additional functionality

--- R/speed_test.R
+++ R/speed_test.R
@@ -2,14 +2,36 @@
 #' 
 #' @author Stuart K. Grange
 #' 
+#' @param n Number of times to run the speed test. 
+#' 
+#' @param sleep If \code{n} is not 1, the number of seconds to sleep before 
+#' speed tests. 
+#' 
 #' @param verbose Should the function give messages? 
 #' 
 #' @return Tibble.
 #' 
 #' @export
-speed_test <- function(verbose = TRUE) {
+speed_test <- function(n = 1, sleep = NA, verbose = TRUE) {
   
-  # To-do: Test for programme
+  # If n is 1, no sleep is needed
+  if (n == 1 && !is.na(sleep)) {
+    sleep <- NA
+  }
+  
+  # Do
+  df <- purrr::map_dfr(
+    seq_len(n), ~speed_test_worker(sleep = sleep, verbose = verbose)
+  )
+  
+  return(df)
+  
+}
+
+
+speed_test_worker <- function(sleep, verbose) {
+  
+  # TODO: Test for programme
   
   # Get system date
   date_system <- lubridate::now()
@@ -20,30 +42,30 @@ speed_test <- function(verbose = TRUE) {
   }, error = function(e) {
     NULL
   })
-  
+
   # If fails return here
   if (is.null(list_speed)) return(tibble())
-  
+
   # Format text output
-  text_output <- list_speed$stdout %>% 
-    stringr::str_split("\n") %>% 
+  text_output <- list_speed$stdout %>%
+    stringr::str_split("\n") %>%
     .[[1]]
-  
+
   # Format the pieces
-  text_download <- text_output %>% 
-    stringr::str_subset("Download") %>% 
+  text_download <- text_output %>%
+    stringr::str_subset("Download") %>%
     stringr::str_split_fixed(": | ", 3)
-  
-  text_upload <- text_output %>% 
-    stringr::str_subset("Upload") %>% 
+
+  text_upload <- text_output %>%
+    stringr::str_subset("Upload") %>%
     stringr::str_split_fixed(": | ", 3)
-  
-  host <- text_output %>% 
-    stringr::str_subset("Testing from") %>% 
+
+  host <- text_output %>%
+    stringr::str_subset("Testing from") %>%
     stringr::str_remove_all("Testing from |\\.\\.\\.")
-  
+
   server <- stringr::str_subset(text_output, "Hosted by")
-  
+
   # Build tibble
   df <- tibble(
     date = date_system,
@@ -54,6 +76,11 @@ speed_test <- function(verbose = TRUE) {
     download = as.numeric(text_download[, 2]),
     upload = as.numeric(text_upload[, 2])
   )
+
+  # Sleep between runs
+  if (!is.na(sleep)) {
+    Sys.sleep(sleep)
+  }
   
   return(df)
   
