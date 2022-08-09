064a57fb4d90eb384e9534861daf4802abf6796c
skgrange <skgrange@gmail.com>
2022-08-09 19:53:23 +0200

    :sparkles: fix r_version

--- R/r_version.R
+++ R/r_version.R
@@ -1,8 +1,8 @@
-#' Function to return a formatred \code{R.Version} call. 
+#' Function to return a formatted \code{R.Version} call. 
 #' 
 #' @author Stuart K. Grange
 #' 
-#' @return Tibble with one row. 
+#' @return Tibble with one row.
 #' 
 #' @export
 r_version <- function() {
@@ -11,15 +11,15 @@ r_version <- function() {
   list_version <- R.Version()
   
   # Build data frame
-  df <- threadr::build_zero_row_tibble(names(list_version))
+  df <- threadr::build_zero_row_tibble(names(list_version), base = TRUE)
   df[1, ] <- as.character(list_version)
   
-  # Clean names
-  names(df) <- stringr::str_replace_all(names(df), "\\.| ", "_")
-  
-  # Add some formatted strings too
+  # Clean names and add some formatted strings
   df <- df %>% 
-    mutate(version_string_short = stringr::str_c(language, " ", major, ".", minor),
+    as_tibble() %>% 
+    dplyr::rename_with(~stringr::str_replace_all(., "\\.| ", "_")) %>% 
+    mutate(version_major_minor = stringr::str_c(major, ".", minor),
+           version_string_short = stringr::str_c(language, " ", version_major_minor),
            version_string_short = stringr::str_c(version_string_short, " ", nickname))
   
   return(df)
--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,7 @@ if (getRversion() >= "2.15.1") {
   # What variables are causing issues?
   variables <- c(
     ".", "variable", "date_start", "value", "bus", "device", "language", "major", 
-    "minor", "version_string_short", "nickname"
+    "minor", "version_string_short", "nickname", "version_major_minor"
   )
   
   # Squash the note
