5fd6926976060ce885642a1a7d195719e68c0864
skgrange <s.k.grange@gmail.com>
2026-06-15 20:07:36 +0200

    :sparkles: update and vectorise image_update_creation_date

--- R/image_update_creation_date.R
+++ R/image_update_creation_date.R
@@ -1,44 +1,60 @@
-#' Function to update an image's creation dates. 
+#' Function to update images' creation dates. 
 #' 
-#' @param file File name.
+#' @param file Vector of image files.
 #' 
-#' @param date Creation date to update \code{file} with.
+#' @param date Vector of dates to update \code{file}.
 #' 
 #' @param verbose Should the function give messages? 
+#' 
+#' @param progress Should a progress bar be displayed? 
 #' 
 #' @author Stuart K. Grange.
 #' 
 #' @return Invisible \code{file}. 
 #' 
 #' @export
-image_update_creation_date <- function(file, date, verbose = FALSE) {
+image_update_creation_date <- function(file, date, verbose = FALSE, 
+                                       progress = FALSE) {
   
-  # Check if programme is installed
-  detect_exiftool()
+  # Check if exiftool is installed
+  is_exiftool_installed()
   
-  # Message to user
-  if (verbose) message(threadr::date_message(), "`", file, "`...")
-  
-  # Check date
+  # Check if date is parsed
   stopifnot(lubridate::is.POSIXt(date))
   
-  # Expand path for system calls
-  file <- normalizePath(file)
+  # Do
+  x <- purrr::walk2(
+    file, date, 
+    ~image_update_creation_date_worker(file = .x, date = .y, verbose = verbose),
+    .progress = progress
+  )
   
-  # Format for metadata
-  date_string <- format(date, format = "%Y:%m:%d %H:%M:%OS%z")
+  return(invisible(x))
+  
+}
+
+
+image_update_creation_date_worker <- function(file, date, verbose) {
+  
+  # Message to user
+  if (verbose) {
+    cli::cli_alert_info("{threadr::cli_date()} `{file}`...")
+  }
+  
+  # Format date for metadata
+  date_format <- format(date, format = "%Y:%m:%d %H:%M:%OS%z")
   
   # Update date time original
-  command_date_time_original <- stringr::str_c("-datetimeoriginal=", date_string)
-  command_create_date <- stringr::str_c("-createdate=", date_string)
+  command_date_time_original <- stringr::str_c("-datetimeoriginal=", date_format)
+  command_create_date <- stringr::str_c("-createdate=", date_format)
   
   # Update date time original
   processx::run(
     "exiftool",
     args = c(command_date_time_original, file, "-overwrite_original")
   )
-  
-  # Update creation date
+
+  # Update creation date too
   processx::run(
     "exiftool",
     args = c(command_create_date, file, "-overwrite_original")
@@ -49,9 +65,16 @@ image_update_creation_date <- function(file, date, verbose = FALSE) {
 }
 
 
-detect_exiftool <- function () {
-  text <- suppressWarnings(system("which exiftool", intern = TRUE, 
-                                  ignore.stderr = TRUE))
-  if (length(text) == 0 || !grepl("exiftool", text)) 
-    stop("'exiftool' system programme not detected...", call. = FALSE)
+is_exiftool_installed <- function() {
+  
+  # Check
+  is_installed <- processx::run("which", "exiftool") %>% 
+    .[["stdout"]] %>% 
+    stringr::str_detect("exiftool")
+  
+  # Error if not installed
+  if (!is_installed) {
+    cli::cli_abort("`exiftool` not detected.")
+  }
+  
 }
