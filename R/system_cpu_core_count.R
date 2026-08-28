5c0f4f39672978c5e04b3a14fcb0cdb6d0e9abe5
skgrange <s.k.grange@gmail.com>
2026-08-28 18:03:20 +0200

    :sparkles: add system_cpu_worker_count

--- R/system_cpu_core_count.R
+++ R/system_cpu_core_count.R
@@ -1,11 +1,11 @@
-#' Function to return the system's number of CPU cores. 
+#' Functions to return a system's number of CPU cores. 
 #' 
 #' @author Stuart K. Grange
 #' 
 #' @param logical_cores Should logical cores be included in the core count? 
 #' 
 #' @param max_cores Should the return have a maximum value? This can be useful 
-#' when there are very many cores and logic is being built. 
+#' when there are many cores and logic is being built. 
 #' 
 #' @return Integer vector with length of 1. 
 #' 
@@ -22,4 +22,20 @@ system_cpu_core_count <- function(logical_cores = TRUE, max_cores = NA) {
     x <- if_else(x >= max_cores, max_cores, x) 
   }
   return(x)
+}
+
+
+#' @rdname system_cpu_core_count
+#' @export
+system_cpu_worker_count <- function() {
+  
+  # Get system name
+  system <- hostname()
+  
+  # Switch from hostname to number of cores, logic depends on p- and e-cores
+  dplyr::case_when(
+    system == "x1-carbon" ~ 4L,
+    .default = system_cpu_core_count() - 1L
+  )
+  
 }
